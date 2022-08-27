ad013a63b7d4a88b1e266891275403662c09f0d3
Kenai Carvalho <hccarv@gmail.com>
2022-08-27 03:15:02 -0300

    Atualização do app

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
 const express = require("express");
+const path = require("path");
+
+const checklistRouter = require("./src/routes/checklist");
+const rootRouter = require("./src/routes/index");
+const methodOverride = require("method-override");
+
+require("./config/database");
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method", { methods: ["POST", "GET"] }));
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.set("views", path.join(__dirname, "src/views"));
+app.set("view engine", "ejs");
+
+app.use("/", rootRouter);
+app.use("/checklists", checklistRouter);
+
+app.listen(3000, () => {
+  console.log("O servidor foi iniciado na porta 3000.");
+});
